c4274a3ecfff47a7fe29be3cc923fef86d3aaacb
ashtray <ashrayshetty09@gmail.com>
2020-02-03 20:50:57 +0530

    Added script tag for brush

--- js/canvas.js
+++ js/canvas.js
@@ -2,6 +2,16 @@ const menu_items=[...document.querySelectorAll('#main-menu > div')];
 const sub_item_list=[...document.querySelectorAll('ul.menu')];
 let visible_menu=[];
 
+const canvas=document.getElementById('canvas');
+const ctx=canvas.getContext('2d');
+
+//Tells if the mouse cursor is clicked or if the user is only hovering over the canvas
+let isDrawing=false;
+
+// To keep track of where the start and stop positions of the stroke are
+let lastX=0;
+let lastY=0;
+
 menu_items.forEach(item=>{
     item.querySelector('li').addEventListener('click', ()=>{
         if(visible_menu.length!==0){
@@ -33,7 +43,6 @@ const color_change=document.querySelectorAll(".color");
 color_change.forEach(color=>{
     color.addEventListener('click', ()=>{
         let col=color.style.backgroundColor;
-        document.getElementById('color-select').style.backgroundColor=col;
         back_fore_color.style.backgroundColor=col;
     });
 });
@@ -42,9 +51,7 @@ const func_buttons=[...document.querySelectorAll("#buttons button")];
 
 let script=undefined;
 func_buttons.forEach(button=>{
-    
     button.addEventListener('click', ()=>{
-        button.removeAttribute('id');
         if(script!==undefined){document.body.removeChild(document.body.lastChild);}
         if(button['id']!=='btn-pressed'){
             func_buttons.map(button=>button.removeAttribute('id'));
@@ -55,7 +62,13 @@ func_buttons.forEach(button=>{
                     script.src="./js/pencil.js";
                     document.body.appendChild(script);
                 }
+            }else if(button.firstElementChild.classList.contains('fa-paint-brush')){
+                if(button['id']==='btn-pressed'){
+                    script=document.createElement('script');
+                    script.src="./js/brush.js";
+                    document.body.appendChild(script);
+                }
             }
-        }
+        }else{button.removeAttribute('id');}
     });
 });
