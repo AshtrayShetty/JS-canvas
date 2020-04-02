2ea31b9b86acd2d68a0f15c8340596298742b65e
AshtrayShetty <51413934+AshtrayShetty@users.noreply.github.com>
2020-04-03 00:53:25 +0530

    invert colors feature added (currently very slow, might use web workers. IDK)

--- js/menu.js
+++ js/menu.js
@@ -52,3 +52,14 @@ document.getElementById("flip").addEventListener('click', ()=>{
         canvas.style.left="70px";
     }
 });
+
+function invertColors(){
+    for(let i=0; i<=610; ++i){
+        for(j=0; j<=1466; ++j){
+            let data=ctx.getImageData(j, i, 1, 1).data;
+            if(data[0]===0 && data[1]===0 && data[2]===0 && data[3]===0){ctx.fillStyle=`rgb(${data[0]}, ${data[1]}, ${data[2]})`;}
+            else{ctx.fillStyle=`rgb(${255-data[0]}, ${255-data[1]}, ${255-data[2]})`;}
+            ctx.fillRect(j, i, 1, 1);
+        }
+    }
+}
